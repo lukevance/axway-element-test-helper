460a359d11fc7a2446378d40e8f4bc0303479657
Luke Vance <lukevance90@gmail.com>
2017-02-28 21:12:03 -0700

    added functionality for saving requests to call again in future

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 "use strict";
 
 const cehandler = require('cloudelements-cehandler');
-const element = require('../../elements-bazaar/native/HubSpot.json');
+const fs = require('fs');
+// const inquirer = require('inquirer');
 const chalk = require('chalk');
-
 // store only the arguments after "node index.js"
 const args = process.argv.slice(2);
 
-// Template for Request Data to be passed into the cehandler
-const request_data = {
-  "headers": {
-    "x-vendor-authorization": "Bearer sampleVendorAuthToken", //This header must be what the vendor expects not Cloud Elements apis
-  },
-  "paths": {
-    // "id": "12345"
-  },
-  "operation": {
-    "method": "DELETE",
-    "path": "/hubs/native/campaigns"
-  },
-  "queryParameters": {
-    // "fields": "Id,Website,AccountNumber"
-  },
-  "body": {} // only used in POST/PATCH
+const getElement = (path) => {
+  return require(path);
 };
 
-// The function that handles the request and logs the response
-const makeRequest = () => {
+const executeCall = (element, request_data) => {
   return cehandler.handler(element)(request_data, {
       requestId: 'fake-run-request',
       succeed: o => console.log(JSON.stringify(o, null, 2)),
@@ -39,11 +24,36 @@ const makeRequest = () => {
   );
 };
 
+const buildRequest = (args) => {
+  if (args.length > 1) {
+    //do something
+  }
+};
+
+// The function that handles the request and logs the response
+const makeRequest = (element, request) => {
+  if (request) {
+    if (request.indexOf('.json') === -1) {
+      request = request + '.json';
+    }
+    fs.readdir('./requests', (err, files) => {
+      if (err) throw err;
+      if (files.indexOf(request) >= 0){
+        let request_body = require('./requests/' + request);
+        executeCall(element, request_body);
+      }
+    });
+  } else {
+    console.log("please provide the name of a request object in the '/requests' directory");
+  }
+
+};
+
 // function to list all resources in an element JSON
-const listResources = () => {
+const listResources = (element) => {
   if (element.resources && (element.resources.length > 0)) {
     element.resources.forEach((resource) => {
-      if (resource.path){
+      if (resource.path) {
           console.log(resource.method + ": " + resource.path);
       }
     });
@@ -53,20 +63,30 @@ const listResources = () => {
 if (args.length < 1) {
   console.log(chalk.red('ERROR: please provide a command'));
   console.log();
-  console.log(chalk.yellow('commands: ') + chalk.blue('list') + chalk.yellow(' OR ') + chalk.blue('request'));
+  console.log(chalk.yellow('commands: ') + chalk.blue('list-resources') + chalk.yellow(' | ') + chalk.blue('new-req') + chalk.blue('make-req'));
   console.log(chalk.yellow('    example:'));
   console.log(chalk.yellow('    node index.js list /path/to/element.json'));
 } else if (args.length < 2) {
   console.log(chalk.red('ERROR: please provide an element path'));
   console.log();
   console.log(chalk.yellow('    example:'));
-  console.log(chalk.yellow('    node index.js request /path/to/element.json'));
-} else if (args.indexOf('list') === 0) {
+  console.log(chalk.yellow('    node index.js make-req /path/to/element.json'));
+} else if (args.indexOf('list-resources') === 0) {
   console.log('list resources');
-  listResources();
-} else if (args.indexOf('request') === 0) {
-  console.log('make a request');
-  makeRequest();
+  let element = getElement(args[1]);
+  listResources(element);
+} else if (args.indexOf('make-req') === 0) {
+  // check for both path to element and requestBody name
+  if (args.length >= 3) {
+    let element = getElement(args[1]);
+    let requestBody = args[2];
+    makeRequest(element, requestBody);
+  } else {
+    console.log('please include a request object and an element.json file');
+  }
+} else if (args.indexOf('new-request') === 0) {
+  console.log('build a request object');
+  buildRequest(args[1]);
 } else {
   console.log(args[0] + ' is not currently a supported command');
   console.log('try list or requests');
